bc90e6c93057b0eb0746531527f6620d9e8ce8e2
Rodrigo Dalcin <rodrigo@godynamo.com>
2017-11-28 17:13:52 -0500

    Adds and activates sprockets.

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 activate :dotenv
+activate :sprockets
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
